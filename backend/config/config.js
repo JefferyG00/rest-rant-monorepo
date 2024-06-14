e6eeeefc931afe893999b59f48cd9c75562e090f
Jeffery Grigsby <jeffery.grigsby@yahoo.com>
2024-06-14 14:51:54 -0700

    first part done

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,23 +2,23 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
+    "username": "postgres",
+    "password": "@Star67",
+    "database": "rest_rant_auth",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
+    "username": "postgres",
+    "password": "@Star67",
+    "database": "rest_rant_auth",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": "postgres",
+    "password": "@Star67",
+    "database": "rest_rant_auth",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
